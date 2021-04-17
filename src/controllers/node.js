b2b68c44d44b0df906170dff1534d9db0d43bb9e
script-0 <ndemabekolleemmanuelisaac@gmail.com>
2021-04-17 18:21:22 +0100

    if else

--- src/controllers/node.js
+++ src/controllers/node.js
@@ -4,12 +4,13 @@ var CommunityModel = require('../models/community.js')
 
 exports.list_all = function (req, res) {
     NodeModel.list(function (err, nodes) {
-      console.log("controller");
-      if (err) {
-        res.send(err);
-      }
-      console.log('res', nodes);
-      res.send(nodes);
+        console.log("controller");
+        if (err) {
+            res.send(err);
+        } else {
+            console.log('res', nodes);
+            res.send(nodes);
+        }
     })
 }
 
@@ -32,34 +33,34 @@ exports.create_node = function (req, res) {
     // !!! verify reg.body Fields !!!
     if (!(req.body.community === undefined)) {
         //Retrieving community id using its name ( req.body.community)
-        CommunityModel.findByName(req.body.community,function(error,result){
-            if(error){
+        CommunityModel.findByName(req.body.community, function (error, result) {
+            if (error) {
                 res.send(error)
-            }else{
+            } else {
                 var new_node = new NodeModel({
-                    lat  : req.body.lat,
-                    long : req.body.long,
-                    community : result.id
+                    lat: req.body.lat,
+                    long: req.body.long,
+                    community: result.id
                 })
-                NodeModel.create(new_node,function(err,node){
-                    if(err){
+                NodeModel.create(new_node, function (err, node) {
+                    if (err) {
                         res.send(err)
-                    }else{
-                       res.json(node)
+                    } else {
+                        res.json(node)
                     }
                 })
             }
         })
-    }else{
+    } else {
         var new_node = new NodeModel({
-            lat  : req.body.lat,
-            long : req.body.long
+            lat: req.body.lat,
+            long: req.body.long
         })
-        NodeModel.create(new_node,function(err,node){
-            if(err){
+        NodeModel.create(new_node, function (err, node) {
+            if (err) {
                 res.send(err)
-            }else{
-               res.json(node)
+            } else {
+                res.json(node)
             }
         })
     }
@@ -75,16 +76,17 @@ exports.create_node = function (req, res) {
 exports.update_node_position = function (req, res) {
     console.log(req.body)
     var updated_node = new UserModel({
-        lat  : req.body.lat,
-        long : req.body.long 
+        lat: req.body.lat,
+        long: req.body.long
     })
     const id = req.body.id
-  
+
     UserModel.update(id, updated_node, function (err, user) {
-      if (err) {
-        res.send(err)
-      }
-      res.json(user)
+        if (err) {
+            res.send(err)
+        } else {
+            res.json(user)
+        }
     })
 }
 
@@ -96,18 +98,18 @@ exports.update_node_position = function (req, res) {
 */
 exports.update_node_community = function (req, res) {
     console.log(req.body)
-    CommunityModel.findByName(req.body.community, function(error,result){
-        if(error){
+    CommunityModel.findByName(req.body.community, function (error, result) {
+        if (error) {
             res.send(error)
-        }else{
+        } else {
             var new_node = new NodeModel({
-                community : result.id
+                community: result.id
             })
-            NodeModel.update(req.body.id, new_node, function(err,node){
-                if(err){
+            NodeModel.update(req.body.id, new_node, function (err, node) {
+                if (err) {
                     res.send(err)
-                }else{
-                   res.json(node)
+                } else {
+                    res.json(node)
                 }
             })
         }
@@ -121,9 +123,10 @@ exports.update_node_community = function (req, res) {
 */
 exports.delete_node = function (req, res) {
     NodeModel.remove(req.body.id, function (err, user) {
-      if (err) {
-        res.send(err);
-      }
-      res.json(user);
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(user);
+        }
     })
 }
