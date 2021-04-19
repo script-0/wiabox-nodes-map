f6ff37ef3e2e4d4fb2632b0106ecca7c1759d441
script-0 <ndemabekolleemmanuelisaac@gmail.com>
2021-04-19 14:04:56 +0100

    Adding Map controller , which will manage map view

---
+++ src/controllers/map.js
@@ -0,0 +1,13 @@
+var NodeModel = require('../models/node.js')
+
+exports.list_all = function (req, res) {
+    NodeModel.list(function (err, nodes) {
+        console.log("controller");
+        if (err) {
+            res.send(err);
+        } else {
+            console.log('res', nodes);
+            res.send(nodes);
+        }
+    })
+}
