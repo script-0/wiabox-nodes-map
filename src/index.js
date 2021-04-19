75392650cdb95029bc5d78949a1374fe62a87d23
script-0 <ndemabekolleemmanuelisaac@gmail.com>
2021-04-19 13:35:14 +0100

    Restruturating project for views layer

--- src/index.js
+++ src/index.js
@@ -6,26 +6,13 @@ const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
 const multer = require('multer');
 var upload = multer();
+var path = require('path')
 
 // Configure express
 const app = express()
 
 var cors = require('cors')
 app.use(cors())
-
-// Configure the root route
-app.get('/', (req, res) => {
-  
-});
-
-// Initalize body parser to parse JSON objects
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-// Initialize authentication system
-// var auth = require('./auth/auth.js')
-// auth(app)
 
 // Initialize routes
 var platformRoutes = require("./routes/platform.js")
@@ -34,6 +21,25 @@ var communityRoutes = require("./routes/community.js")
 platformRoutes(app)
 nodeRoutes(app)
 communityRoutes(app)
+
+//Configure EJS HTML rendering
+app.set("view engine", "ejs")
+
+//Configure static and folders
+app.use("/assets",express.static(path.join(__dirname, "../public/")))
+app.set('views', path.join(__dirname, '/views/'));
+
+console.log("--dirname = " + __dirname)
+
+// Configure the root route
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+// Initalize body parser to parse JSON objects
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 // Launch express server on port 3000
 app.listen(process.env.PORT_LISTEN);
